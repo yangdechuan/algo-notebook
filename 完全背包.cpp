ad6f5a289c33328b78b84a313b0c37a820280073
captain <yangdechuan@hotmail.com>
2019-08-21 18:43:15 +0800

    modify annotation

--- 完全背包.cpp
+++ 完全背包.cpp
@@ -1,5 +1,12 @@
-#include<cstdio>
-#include<algorithm>
+/*
+有 N 种物品和一个容量是 V 的背包，每种物品都有无限件可用。
+
+第 i 种物品的体积是 vi，价值是 wi。
+
+求解将哪些物品装入背包，可使这些物品的总体积不超过背包容量，且总价值最大。
+*/
+#include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
@@ -10,15 +17,16 @@ int v[1000 + 1], w[1000 + 1];
 // dp[j] 表示背包容量不超过j的最大价值
 int dp[1000 + 1];
 
-int main(){
+int main() {
     scanf("%d%d", &N, &V);
-    for(int i = 1; i <= N; i++){
+    for (int i = 1; i <= N; i++) {
         scanf("%d%d", &v[i], &w[i]);
     }
-    for(int i = 0; i <= V; i++) dp[i] = 0;
+    for (int i = 0; i <= V; i++) dp[i] = 0;
     // DP
-    for(int i = 1; i <= N; i++){
-        for(int j = v[i]; j <= V; j++){ // 与01背包唯一不同地方，从前往后计算。
+    for (int i = 1; i <= N; i++) {
+        for (int j = v[i]; j <= V;
+             j++) {  // 与01背包唯一不同地方，从前往后计算。
             dp[j] = max(dp[j], dp[j - v[i]] + w[i]);
         }
     }
