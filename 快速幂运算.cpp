f793e5f51dabacb2015ca5432032e377df58d74e
captain <yangdechuan@hotmail.com>
2019-08-29 21:51:51 +0800

    kuaisumi

--- 快速幂运算.cpp
+++ 快速幂运算.cpp
@@ -5,7 +5,7 @@ using namespace std;
 求x的n次幂
 时间复杂度：O(log(n))
 */
-int pow(int x, int n) {
+int pow1(int x, int n) {
     int ans = 1;
     while (n) {
         if (n & 1) ans = ans * x;
@@ -15,7 +15,14 @@ int pow(int x, int n) {
     return ans;
 }
 
+int pow2(int x, int n) {
+    if (n == 0) return 1;
+    int ans = pow2(x * x, n / 2);
+    if (n & 1) ans = ans * x;
+    return ans;
+}
+
 int main() {
-    cout << pow(2, 5) << endl;
+    cout << pow2(2, 5) << endl;
     return 0;
 }
