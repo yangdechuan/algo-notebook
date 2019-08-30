69b452c4b2b529b94718697085fe9c8884d462cf
captain <yangdechuan@hotmail.com>
2019-08-30 12:42:42 +0800

    modify head sort

--- 堆排序.cpp
+++ 堆排序.cpp
@@ -1,7 +1,7 @@
 /*
 最坏情况时间复杂性：O(n*log(n))
 平均情况时间复杂性：O(n*log(n))
-稳定性：快速排序是不稳定的排序方法
+稳定性：堆排序是不稳定的排序方法
 */
 #include <iostream>
 #include <queue>
@@ -14,7 +14,8 @@ vector<int> heapSort(vector<int>& nums) {
     // 建小根堆, 时间复杂度：O(n)
     // 注意:priority_queue默认是大根堆
     // 这里我们建小根堆
-    priority_queue<int, vector<int>, greater<int>> que(nums.begin(), nums.end());
+    priority_queue<int, vector<int>, greater<int>> que(nums.begin(),
+                                                       nums.end());
     // 每次从堆中取最小元素 O(n*log(n))
     for (int i = 0; i < n; i++) {
         ans[i] = que.top();
