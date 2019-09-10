59ae61170ef25bb76364ea6c8258381f8933daff
captain <yangdechuan@hotmail.com>
2019-09-10 18:37:17 +0800

    merge math

--- 数论/素数.cpp
+++ 数论/素数.cpp
@@ -1,15 +1,15 @@
-#include<vector>
-#include<iostream>
-#include<cmath>
-#include<algorithm>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 /*
 素数测试 O(sqrt(n))
 */
 bool isPrime(int n) {
-    for(int i = 2; i * i <= n; i++){
-        if(n % i == 0) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
     }
     return n != 1;
 }
@@ -23,13 +23,13 @@ bool isPrime(int n) {
 以此类推，如果表中剩余的最小数字是m时，m就是素数，然后将表中所有m的倍数都划去。
 像这样反复操作，就能依次枚举n以内的素数
 */
-vector<int> sieve(int n){
+vector<int> sieve(int n) {
     vector<bool> is_prime(n, true);
     vector<int> ans;
-    for(int i = 2; i <= n; i++){
-        if(is_prime[i]){
+    for (int i = 2; i <= n; i++) {
+        if (is_prime[i]) {
             ans.push_back(i);
-            for(int j = i + i; j <= n; j += i){
+            for (int j = i + i; j <= n; j += i) {
                 is_prime[j] = false;
             }
         }
@@ -42,36 +42,36 @@ vector<int> sieve(int n){
 区间筛法：
 根据sqrt(b)以内的素数，将埃氏筛法运用在[a, b）上。
 */
-vector<int> segment_sieve(int a, int b){
+vector<int> segment_sieve(int a, int b) {
     int sqr_b = sqrt(b);
     vector<bool> is_prime_small(sqr_b + 1, true);
     vector<bool> is_prime_big(b - a, true);
     // [2, sqrt(b)）
-    for(int i = 2; i * i < b; i++) is_prime_small[i] = true;
+    for (int i = 2; i * i < b; i++) is_prime_small[i] = true;
     // 【a, b)
-    for(int i = 0; i < b - a; i++) is_prime_big[i] = true;
-    
-    for(int i = 2; i * i < b; i++){
-        if(is_prime_small[i]){
+    for (int i = 0; i < b - a; i++) is_prime_big[i] = true;
+
+    for (int i = 2; i * i < b; i++) {
+        if (is_prime_small[i]) {
             // 划去[2, sqrt(b)）的非质数
-            for(int j = i + i; j * j < b; j += i) is_prime_small[j] = false;
+            for (int j = i + i; j * j < b; j += i) is_prime_small[j] = false;
             // 划去[a, b)的非质数
             // (a + i - 1) / i) * i是第一个大于等于a的i的倍数
-            for(int j = max(2, (a + i - 1) / i) * i; j < b; j += i){
+            for (int j = max(2, (a + i - 1) / i) * i; j < b; j += i) {
                 is_prime_big[j - a] = false;
             }
         }
     }
     vector<int> ans;
-    for(int i = 0; i < b - a; i++){
-        if(is_prime_big[i] && a + i >= 2) ans.push_back(a + i);
+    for (int i = 0; i < b - a; i++) {
+        if (is_prime_big[i] && a + i >= 2) ans.push_back(a + i);
     }
     return ans;
 }
 
-int main(){
+int main() {
     vector<int> ans = segment_sieve(11, 19);
-    for(int i = 0; i < ans.size(); i++){
+    for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << endl;
     }
 }
