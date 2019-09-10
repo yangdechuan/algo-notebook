b8575d662535809b33cd0deb69c636f32db94551
captain <yangdechuan@hotmail.com>
2019-09-10 13:57:29 +0800

    modify quick sort

--- 快速排序.cpp
+++ 快速排序.cpp
@@ -8,25 +8,49 @@
 
 using namespace std;
 
-vector<int> nums;
-void qsort(int left, int right) {
-    if (left >= right) return;
-    int i = left, j = right + 1;
-    int tmp = nums[left];  // 算法导论中叫它主元（pivot element）
-    while (i < j) {
+vector<int> nums = {1, 5, 2, 2, 4, 3, 9, 2};
+
+int partition(int left, int right) {
+    int pivot = nums[left];
+    int i = left;
+    for (int j = left + 1; j <= right; j++) {
+        if (nums[j] < pivot) {
+            i++;
+            swap(nums[i], nums[j]);
+        }
+    }
+    swap(nums[i], nums[left]);
+    return i;
+}
+// 另一种做法
+int partition2(int left, int right) {
+    int pivot = nums[left];  // 算法导论中叫它主元（pivot element）
+    int i = left + 1, j = right;
+    while (i <= j) {
         // 从左往右，找第一个（大于）tmp的位置
-        i++;
-        while (i <= right && nums[i] <= tmp) i++;
+        while (i <= right && nums[i] <= pivot) i++;
         // 从右往左，找第一个（小于等于）tmp的位置
-        j--;
-        while (nums[j] > tmp) j--;
+        while (nums[j] > pivot) j--;
         // 交换i和j位置的元素
         if (i < j) swap(nums[i], nums[j]);
     }
     // 此时j是从右往左第一个小于等于nums[left]的位置;
     // 并且j左侧的元素一定都小于等于nums[left]（理由是此时i在j的右侧，并且i左侧元素小于等于nums[left]）
     swap(nums[left], nums[j]);
+    return j;
+}
+
+void qsort(int left, int right) {
+    if (left >= right) return;
+    int q = partition(left, right);
     // 此时j左侧都是小于等于它的元素，右侧都是大于它的元素
-    qsort(left, j - 1);
-    qsort(j + 1, right);
+    qsort(left, q - 1);
+    qsort(q + 1, right);
+}
+
+int main() {
+    qsort(0, nums.size() - 1);
+    for (int x : nums) {
+        cout << x << " ";
+    }
 }
