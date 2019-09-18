ce8c826ec1fb0c406a74005b3fe0f3f7652c84ff
captain <yangdechuan@hotmail.com>
2019-09-18 12:37:39 +0800

    modify reverse list

--- 链表/链表反转.cpp
+++ 链表/链表反转.cpp
@@ -1,10 +1,9 @@
-#include<cstdio>
-
+#include <cstdio>
 
 // Definition for singly-linked list.
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
@@ -16,11 +15,11 @@ Input: 1->2->3->4->5->NULL
 Output: 5->4->3->2->1->NULL
  */
 class Solution {
-public:
+   public:
     ListNode* reverseList(ListNode* head) {
         ListNode* pre = NULL;
         ListNode* cur = head;
-        while(cur){
+        while (cur) {
             ListNode* nextNode = cur->next;
             cur->next = pre;
             pre = cur;
@@ -38,20 +37,20 @@ Input: 1->2->3->4->5->NULL, m = 2, n = 4
 Output: 1->4->3->2->5->NULL
  */
 class Solution {
-public:
+   public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
-        if(n <= m) return head;
+        if (n <= m) return head;
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
         ListNode* pre = dummy;
         // pre为反转子链表的前一个结点
-        for(int i = 0; i < m - 1; i++){
+        for (int i = 0; i < m - 1; i++) {
             pre = pre->next;
         }
         // 从pre->next开始反转，做n-m步。
         ListNode* first = pre->next;
         ListNode* second = first->next;
-        for(int i = 0; i < (n - m); i++){
+        for (int i = 0; i < (n - m); i++) {
             ListNode* nextNode = second->next;
             second->next = first;
             first = second;
@@ -72,9 +71,9 @@ For k = 2, you should return: 2->1->4->3->5
 For k = 3, you should return: 3->2->1->4->5
  */
 class Solution {
-public:
+   public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(k <= 1) return head;
+        if (k <= 1) return head;
 
         // 小技巧：添加一个辅助结点，使得第一个要反转的子链表也有前驱结点。
         ListNode* dummy = new ListNode(0);
@@ -83,18 +82,18 @@ public:
         // 定义两个指针分别指向要反转的list的首尾，这样list反转之后容易找到首尾
         // 还要定义一个指针指向这段list的前一个元素（cur）
         ListNode* cur = dummy;
-        while(cur){
-            head = cur->next;  // 指向这段子链表的头
+        while (cur) {
+            head = cur->next;      // 指向这段子链表的头
             ListNode* tail = cur;  // 指向这段子链表的尾
-            for(int i = 0; i < k; i++){
+            for (int i = 0; i < k; i++) {
                 tail = tail->next;
-                if(tail == NULL) break;
+                if (tail == NULL) break;
             }
-            if(tail == NULL) break;
+            if (tail == NULL) break;
             // 以下为反转[head, tail]这段子链表操作
             ListNode* first = head;
             ListNode* second = first->next;
-            while(first != tail){
+            while (first != tail) {
                 ListNode* nextNode = second->next;
                 second->next = first;
                 first = second;
