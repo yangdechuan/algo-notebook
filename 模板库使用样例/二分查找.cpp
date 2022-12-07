e33eed6b660a427603e28e9b0596a42f8e29ce5d
yangdechuan <yangdechuan@bytedance.com>
2022-12-07 21:26:19 +0800

    Add binary search

--- 模板库使用样例/二分查找.cpp
+++ 模板库使用样例/二分查找.cpp
@@ -1,35 +1,72 @@
-#include<algorithm>
-#include<vector>
-#include<iostream>
-
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    // 1. binary_search返回bool
-    vector<int> a = {2, 2, 4, 4, 6, 6, 8, 9, 10, 10};
-    // 是否含有5
-    if(binary_search(a.begin(), a.end(), 6)){
-        cout << "Found!" << endl;
-    }
 
-    // 2. equal_range()返回pair<>
-    pair<vector<int>::iterator,vector<int>::iterator> bounds = equal_range(a.begin(), a.end(), 4);
-    cout << "bounds at positions " << (bounds.first - a.begin());
-    cout << " and " << (bounds.second - a.begin()) << '\n';
+void test_bound() {
+    // lower_bound(first, last, value) 返回第一个大于等于val的位置
+    // upper_bound(first, last, value) 返回第一个大于val的位置
+    // 如果value在范围内，那么 [lower_bound, upper_bound) 区间等于value
+    // 如果value不在范围内，那么 lower_bound=upper_bound
+    vector<int> vec = {1, 1, 2, 2, 4, 4};
 
-    // 3. lower_bound()和upper_bound()返回iterator
-    int myints[] = {10,20,30,30,20,10,10,20};
-    vector<int> vec(myints, myints + 8);
-    sort(vec.begin(), vec.end());   
-    vector<int>::iterator low,up;
-    // 返回第一个大于等于20的位置
-    low = lower_bound (vec.begin(), vec.end(), 20);
-    // 返回第一个大于20的位置
-    up = upper_bound (vec.begin(), vec.end(), 20);
+    int val = 2;
+    auto it1 = lower_bound(vec.begin(), vec.end(), val);
+    auto it2 = upper_bound(vec.begin(), vec.end(), val);
+    assert(it1 - vec.begin() == 2);
+    assert(it2 - vec.begin() == 4);
+    
+    val = 0;
+    it1 = lower_bound(vec.begin(), vec.end(), val);
+    it2 = upper_bound(vec.begin(), vec.end(), val);
+    assert(it1 - vec.begin() == 0);
+    assert(it2 - vec.begin() == 0);
 
-    cout << "lower_bound at position " << (low- vec.begin()) << '\n';
-    cout << "upper_bound at position " << (up - vec.begin()) << '\n';
-    // lower_bound at position 3
-    // upper_bound at position 6
+    val = 5;
+    it1 = lower_bound(vec.begin(), vec.end(), val);
+    it2 = upper_bound(vec.begin(), vec.end(), val);
+    assert(it1 == vec.end());
+    assert(it2 == vec.end());
+}
+
+
+void test_range() {
+    // equal_range(first, last, value) 返回一个 pair，分别对应lower_bound和upper_bound的返回结果
+    vector<int> vec = {1, 1, 2, 2, 4, 4};
+
+    int val = 2;
+    pair<vector<int>::iterator, vector<int>::iterator> bounds = equal_range(vec.begin(), vec.end(), val);
+    assert(bounds.first - vec.begin() == 2);
+    assert(bounds.second - vec.begin() == 4);
+
+    val = 0;
+    bounds = equal_range(vec.begin(), vec.end(), val);
+    assert(bounds.first - vec.begin() == 0);
+    assert(bounds.second - vec.begin() == 0);
+
+    val = 5;
+    bounds = equal_range(vec.begin(), vec.end(), val);
+    assert(bounds.first == vec.end());
+    assert(bounds.second == vec.end());
+}
+
+
+void test_binary() {
+    // binary_search(first, last, value) 判断value是否在范围内
+    vector<int> vec = {1, 1, 2, 2, 4, 4};
+
+    bool flag = binary_search(vec.begin(), vec.end(), 1);
+    assert(flag == true);
+
+    flag = binary_search(vec.begin(), vec.end(), 3);
+    assert(flag == false);
+}
+
+
+int main() {
+    test_bound();
+    test_range();
+    test_binary();
 }
