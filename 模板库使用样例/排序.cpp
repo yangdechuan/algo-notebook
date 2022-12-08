affc695a2b6e58383a073f25fce1eb1dca9005d7
yangdechuan <yangdechuan@bytedance.com>
2022-12-08 11:41:53 +0800

    Modify sort

--- 模板库使用样例/排序.cpp
+++ 模板库使用样例/排序.cpp
@@ -1,30 +1,52 @@
-#include <iostream>     // std::cout
-#include <algorithm>    // std::sort
-#include <vector>       // std::vector
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
 using namespace std;
 
-bool comp (int i,int j) {
-    return i > j;
+
+vector<int>::iterator my_partition(vector<int>::iterator first, vector<int>::iterator last, function<bool(int)> pred) {
+    auto fast = first;
+    auto slow = first;
+    while(fast < last) {
+        if(pred(*fast)) {
+            swap(*fast, *slow);
+            slow++;
+        }
+        fast++;
+    }
+    return slow;
 }
 
-int main () {
-    int a[8] = {32, 71, 12, 45, 26, 80, 53, 33};
-    sort(a, a + 8);  // 默认升序
-    for(int i : a){
-      cout << i << " ";
-    }
-    cout << endl;
 
-    vector<int> vec = {2, 4, 9, 6, 5};
-    sort(vec.begin(), vec.end(), comp);  // 降序
-    for(int i: vec){
-      cout << i << " ";
+void my_quick_sort(vector<int>::iterator first, vector<int>::iterator last) {
+    if(first >= last) {
+        return;
+    }
+    int pivot = *first;
+    // 定义一个lambda表达式
+    auto pred = [=](int x) { return x < pivot; };
+    // 分割。重排这个范围，使得谓语对其返回true的元素在前，返回false的在后，并返回指向第二组元素首元素的迭代器
+    // 模板库 partition 算法
+    // auto mid = partition(first + 1, last, pred);
+    // 复现 partition
+    auto mid = my_partition(first + 1, last, pred);
+    swap(*first, *(mid - 1));
+    my_quick_sort(first, mid - 1);
+    my_quick_sort(mid, last);
+}
+
+
+int main() {
+    vector<int> vec = {6, 1, 2, 1, 6, 3, 8, 5};
+    // 排序
+    // 模板库 sort 算法
+    // sort(vec.begin(), vec.end());
+    // 复现 sort
+    my_quick_sort(vec.begin(), vec.end());
+    for(int i = 0; i < vec.size(); i++) {
+        cout << vec[i] << " ";
     }
     cout << endl;
-    return 0;
-    /*
-    输出结果：
-    12 26 32 33 45 53 71 80 
-    9 6 5 4 2 
-     */
 }
